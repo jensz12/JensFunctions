5c1c501c9968bb35d6ca83b33a47166ab700f0fc
Jens Møller <jensz123@gmail.com>
2021-03-12 08:46:13 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'cronjob_key' => '',
+	'youtube_key' => '',
+	'youtube_id' => '',
+	'youtube_video_count' => ,
+	'twitch_client_id' => '',
+	'twitch_oauth_token' => '',
+	'twitch_id' => '',
+	'discord_widget_id' => ''
+);
+?>
